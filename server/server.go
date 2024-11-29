8801cce4536b810439ffc5c5d52ec6fdfdd376ca
letsmakecakes <adwaithrajeev@gmail.com>
2024-11-29 17:10:13 +0530

    Added method to construct a server object.

--- server/server.go
+++ server/server.go
@@ -1,2 +1,38 @@
 // Package server contains everything for setting up and running the HTTP server.
 package server
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type Server struct {
+	address string
+	mux     chi.Router
+	server  *http.Server
+}
+
+type Options struct {
+	Host string
+	Port int
+}
+
+func New(opts Options) *Server {
+	address := net.JoinHostPort(opts.Host, strconv.Itoa(opts.Port))
+	mux := chi.NewMux()
+	return &Server{
+		address: address,
+		mux:     mux,
+		server: &http.Server{
+			Addr:              address,
+			Handler:           mux,
+			ReadTimeout:       5 * time.Second,
+			ReadHeaderTimeout: 5 * time.Second,
+			WriteTimeout:      5 * time.Second,
+			IdleTimeout:       5 * time.Second,
+		},
+	}
+}
